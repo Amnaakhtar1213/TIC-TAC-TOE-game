b96b6a402a78247f83e61522db76772799fb8795
amnaakhtar1213 <amnabinterasheed1911@gmail.com>
2026-05-23 04:47:46 +0500

    solve some fault in js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const winPatterns = [
 ];
 
 const resetGame = () => {
-turno = true;
+turnO = true;
 enabledBoxes ();
 msgContainer.classList.add("hide");
 
@@ -62,13 +62,13 @@ const showWinner = (winner) => {
 
 
     const checkWinner = () => {
-      for(pattern of winPatterns) {
+      for(let pattern of winPatterns) {
          let pos1Val = boxes[pattern[0]].innerText;
          let pos2Val = boxes[pattern[1]].innerText;
          let pos3Val = boxes[pattern[2]].innerText;
 
          if(pos1Val != "" && pos2Val != "" && pos3Val != "") {
-          if(pos1Val === pos2Val, pos2Val === pos3Val) { 
+          if(pos1Val === pos2Val && pos2Val === pos3Val) { 
           showWinner(pos1Val);       
          }
       }
